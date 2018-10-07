220070a8a85fb0df7103f82bff1431a8a0d1541a
Gabriel Plassard <gabriel.plassard@gmail.com>
2018-10-07 18:44:38 +0200

    produce json

---
+++ src/parse.js
@@ -0,0 +1,25 @@
+const cheerio = require('cheerio');
+const fs = require('fs');
+
+const sessions = [];
+
+fs.readdirSync("sessions").forEach(fileName => {
+    const [day, hotel] = fileName.split("_"); //?
+
+    const file = fs.readFileSync('sessions/' + fileName, {encoding: 'utf-8'});
+    const $ = cheerio.load(file)
+        
+    $('.sessionRow').each((index, elem) => {
+        const id = $(elem).attr('id') //?
+        const abbr = $('.abbreviation', elem).text() //?
+        const title = $('.title', elem).text() //?
+        const abstract = $('.abstract', elem).text() //?
+        const type = $('.type', elem).text() //?
+        sessions.push({id, abbr, title, abstract, type, day, hotel: hotel.replace(".html", "")})
+    })
+})
+
+sessions.length//?
+
+
+fs.writeFileSync('sessions_json/all.json', JSON.stringify(sessions, null, 4))
